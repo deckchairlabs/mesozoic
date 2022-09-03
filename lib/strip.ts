fd5a75116c9ad24f9f8d909c6f4b06fbdbdfa55f
James Edmonds <james@deckchairlabs.com>
2022-09-03 05:52:24 +0000

    fix: better conditional expression stripping

--- lib/strip.ts
+++ lib/strip.ts
@@ -1,9 +1,12 @@
 import { parse, print, Types, Visitor } from "./swc.ts";
 
-export async function stripMesozoicConditionals(
+export async function stripConditionalIfStatements(
   content: string,
+  conditional: string,
   minify?: boolean,
 ) {
+  const parsedConditional = await parseExpressionStatement(conditional);
+
   const parsed = await parse(content, {
     syntax: "typescript",
     tsx: true,
@@ -11,7 +14,11 @@ export async function stripMesozoicConditionals(
     dynamicImport: true,
   });
 
-  const stripped = new Stripper().visitProgram(parsed);
+  const stripped = new ConditionalExpressionStripper(parsedConditional)
+    .visitProgram(
+      parsed,
+    );
+
   const result = await print(stripped, {
     minify,
     jsc: {
@@ -24,23 +31,117 @@ export async function stripMesozoicConditionals(
   return result.code;
 }
 
-class Stripper extends Visitor {
+export async function parseExpressionStatement(
+  statement: string,
+): Promise<Types.ExpressionStatement | undefined> {
+  const parsed = await parse(statement, {
+    syntax: "ecmascript",
+    script: true,
+    jsx: false,
+  });
+
+  if (parsed.body && parsed.body[0].type === "ExpressionStatement") {
+    return parsed.body[0];
+  }
+}
+
+class ConditionalExpressionStripper extends Visitor {
+  constructor(private conditional: Types.ExpressionStatement | undefined) {
+    super();
+  }
+
   visitIfStatement(statement: Types.IfStatement): Types.Statement {
-    if (
-      statement.test.type === "Identifier" &&
-      statement.test.value === "__MESOZOIC_BUILD__"
-    ) {
-      if (statement.alternate) {
-        return statement.alternate;
+    if (this.conditional) {
+      let replaceStatement = false;
+
+      if (
+        this.isBinaryExpression(statement.test) &&
+        this.isBinaryExpression(this.conditional.expression)
+      ) {
+        replaceStatement = this.#testBinaryExpressionAgainstConditional(
+          statement.test,
+          this.conditional.expression,
+        );
+      } else if (
+        this.isIdentifier(statement.test) &&
+        this.isIdentifier(this.conditional.expression)
+      ) {
+        replaceStatement = this.#testIdentifiersMatch(
+          statement.test,
+          this.conditional.expression,
+        );
       }
-      return {
-        type: "EmptyStatement",
-        span: statement.span,
-      };
+
+      if (replaceStatement) {
+        if (statement.alternate) {
+          return statement.alternate;
+        }
+
+        return {
+          type: "EmptyStatement",
+          span: statement.span,
+        };
+      }
     }
+
     return super.visitIfStatement(statement);
   }
+
+  isBinaryExpression(
+    expression: Types.Expression | undefined,
+  ): expression is Types.BinaryExpression {
+    if (!expression) {
+      return false;
+    }
+
+    return expression.type === "BinaryExpression";
+  }
+
+  isIdentifier(expression: Types.Expression): expression is Types.Identifier {
+    return expression.type === "Identifier";
+  }
+
   visitTsType(type: Types.TsType): Types.TsType {
     return type;
+  }
+
+  #testIdentifiersMatch(
+    identifier1: Types.Identifier,
+    identifier2: Types.Identifier,
+  ) {
+    return identifier1.value === identifier2.value;
+  }
+
+  #testStringLiteralsMatch(
+    literal1: Types.StringLiteral,
+    literal2: Types.StringLiteral,
+  ) {
+    return literal1.value === literal2.value;
+  }
+
+  #testExpressionsMatch(
+    exp1: Types.Expression,
+    exp2: Types.Expression,
+  ) {
+    if (this.isIdentifier(exp1) && this.isIdentifier(exp2)) {
+      return this.#testIdentifiersMatch(exp1, exp2);
+    }
+
+    if (exp1.type === "StringLiteral" && exp2.type === "StringLiteral") {
+      return this.#testStringLiteralsMatch(exp1, exp2);
+    }
+
+    return false;
+  }
+
+  #testBinaryExpressionAgainstConditional(
+    expression: Types.BinaryExpression,
+    conditional: Types.BinaryExpression,
+  ) {
+    return [
+      expression.operator === conditional.operator,
+      this.#testExpressionsMatch(expression.left, conditional.left),
+      this.#testExpressionsMatch(expression.right, conditional.right),
+    ].every((value) => value === true);
   }
 }
--- test/strip.test.ts
+++ test/strip.test.ts
@@ -1,27 +1,44 @@
 import { assertEquals } from "./deps.ts";
-import { stripMesozoicConditionals } from "../lib/strip.ts";
+import {
+  parseExpressionStatement,
+  stripConditionalIfStatements,
+} from "../lib/strip.ts";
+import { assertNotEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
 
-Deno.test("it works", { ignore: true }, async () => {
+Deno.test("it works", async () => {
   const source = `
     import React from 'react';
-    if (__MESOZOIC_BUILD__) {
+    if (__DEV__) {
       console.log('removed')
       console.log(React)
     } else {
       console.log('not removed')
     }
-    if (__MESOZOIC_BUILD__) {
+    if (__DEV__) {
       const {default: compiler} = await import('./compiler.ts');
     }
   `;
 
-  const result = await stripMesozoicConditionals(source);
+  const result = await stripConditionalIfStatements(
+    source,
+    "__DEV__",
+  );
 
-  assertEquals(result.includes("React"), false);
+  assertEquals(result.includes("React"), true);
   assertEquals(result.includes("console.log('removed')"), false);
   assertEquals(
     result.includes("compiler"),
     false,
   );
   assertEquals(result.includes("console.log('not removed')"), true);
+});
+
+Deno.test("it can parse expression statements", async () => {
+  const statement = "mode === 'development'";
+  const parsed = await parseExpressionStatement(statement);
+
+  assertNotEquals(parsed, undefined);
+
+  assertEquals(parsed!.type, "ExpressionStatement");
+  assertEquals(parsed!.expression.type, "BinaryExpression");
 });
