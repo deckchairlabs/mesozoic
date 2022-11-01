57555bd0f3f7ad39b4a81cf9ea4c53a32357c7b1
James Edmonds <james@deckchairlabs.com>
2022-11-01 07:16:24 +0000

    chore: update swc

--- lib/swc.ts
+++ lib/swc.ts
@@ -1,19 +1,19 @@
-import init from "https://esm.sh/@swc/wasm-web@1.3.9/wasm-web.js";
+import init from "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web.js";
 export {
   parse,
   print,
   transform,
-} from "https://esm.sh/@swc/wasm-web@1.3.9/wasm-web.js";
+} from "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web.js";
 export type {
   JscTarget,
   ParserConfig,
-} from "https://esm.sh/@swc/wasm-web@1.3.9/wasm-web.js";
-export { Visitor } from "https://esm.sh/@swc/core@1.3.9/Visitor.js";
-export * as Types from "https://esm.sh/@swc/core@1.3.9/types.d.ts";
+} from "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web.js";
+export { Visitor } from "https://esm.sh/@swc/core@1.3.11/Visitor.js";
+export * as Types from "https://esm.sh/@swc/core@1.3.11/types.d.ts";
 import { cache, toFileUrl } from "./deps.ts";
 
 const file = await cache(
-  "https://esm.sh/@swc/wasm-web@1.3.9/wasm-web_bg.wasm",
+  "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web_bg.wasm",
 );
 
 await init(toFileUrl(file.path));
