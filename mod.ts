837f48a98126d4366642ecac52960e41d8e17a61
James Edmonds <james@deckchairlabs.com>
2022-11-01 06:55:33 +0000

    fix: export BuilderOptions type

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 export { Builder } from "./lib/builder.ts";
 export { ContextBuilder } from "./lib/context.ts";
 export type { PatternLike } from "./lib/patterns.ts";
-export type { BuildResult } from "./lib/builder.ts";
+export type { BuilderOptions, BuildResult } from "./lib/builder.ts";
 export type { BuildContext } from "./lib/context.ts";
 export type { ImportMap } from "./lib/types.ts";
 export { FileBag } from "./lib/sources/fileBag.ts";
