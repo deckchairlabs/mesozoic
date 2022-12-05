9d9f199f6696a7d14f819da8388b82e345fbdc51
James Edmonds <james@deckchairlabs.com>
2022-12-05 12:32:54 +0000

    chore: cleanup

--- lib/deps.ts
+++ lib/deps.ts
@@ -2,10 +2,7 @@ export { copy, emptyDir, ensureDir, walk } from "https://deno.land/std@0.164.0/f
 export { cache, exists as existsInCache } from "https://deno.land/x/cache@0.2.13/mod.ts";
 export { crayon } from "https://deno.land/x/crayon@3.3.2/mod.ts";
 export * as log from "https://deno.land/std@0.164.0/log/mod.ts";
-export {
-  createGraph,
-  load as graphDefaultLoad,
-} from "https://deno.land/x/deno_graph@0.38.0/mod.ts";
+export { createGraph } from "https://deno.land/x/deno_graph@0.38.0/mod.ts";
 export { deepMerge } from "https://deno.land/std@0.164.0/collections/deep_merge.ts";
 export {
   init as initModuleLexer,
