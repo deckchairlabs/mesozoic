eb223e3c64de09b508641dbeaa26c42d0bda3a8b
James Edmonds <james@deckchairlabs.com>
2022-12-08 23:26:28 +1030

    feat: added matches api to filebag (#11)

--- test/fileBag.test.ts
+++ test/fileBag.test.ts
@@ -1,7 +1,13 @@
 import { FileBag } from "../lib/sources/fileBag.ts";
-import { assertEquals } from "./deps.ts";
+import { assertEquals, join } from "./deps.ts";
 
 Deno.test("constructor", () => {
   const fileBag = new FileBag();
   assertEquals(fileBag.size, 0);
+});
+
+Deno.test("matches", async () => {
+  const fileBag = await FileBag.from(join(Deno.cwd(), "./test/fixture/app"));
+  const publicFiles = fileBag.matches("./public/**");
+  assertEquals(publicFiles.size, 3);
 });
