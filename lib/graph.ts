a5db3ae7c7eb277dd93c43687f02ddc09f2a6020
James Edmonds <james@deckchairlabs.com>
2023-01-21 01:59:11 +0000

    wip

--- lib/graph.ts
+++ lib/graph.ts
@@ -1,9 +1,9 @@
-import { init } from "https://deno.land/x/deno_graph@0.40.0/mod.ts";
-export { createGraph } from "https://deno.land/x/deno_graph@0.40.0/mod.ts";
+import { init } from "https://deno.land/x/deno_graph@0.41.0/mod.ts";
+export { createGraph } from "https://deno.land/x/deno_graph@0.41.0/mod.ts";
 import { cache, toFileUrl } from "./deps.ts";
 
 const file = await cache(
-  "https://deno.land/x/deno_graph@0.40.0/lib/deno_graph_bg.wasm",
+  "https://deno.land/x/deno_graph@0.41.0/lib/deno_graph_bg.wasm",
 );
 
 await init({ url: toFileUrl(file.path) });
