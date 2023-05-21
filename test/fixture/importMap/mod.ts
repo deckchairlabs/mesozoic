aeb3cd2716e329e0ff23533429620bd23dd47d45
James Edmonds <james@deckchairlabs.com>
2023-05-21 03:23:28 +0000

    chore: cleanup

--- test/fixture/importMap/mod.ts
+++
@@ -1,7 +0,0 @@
-import * as React from "https://esm.sh/react";
-import * as ReactDOM from "https://esm.sh/react-dom";
-import { createServer } from "https://deno.land/x/ultra@v2.0.0-alpha.12/server.ts";
-
-console.log(React, ReactDOM);
-// deno-lint-ignore no-unused-vars
-const server = createServer();
