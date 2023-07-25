84ef4ff358d9b9107a8255a049bb05179c0a9d79
James Edmonds <james@deckchairlabs.com>
2023-07-25 04:32:47 +0000

    v1.3.8

--- version.ts
+++ version.ts
@@ -1,4 +1,4 @@
-export const VERSION = "1.3.7";
+export const VERSION = "1.3.8";
 export const VERSION_REGEX = /\/\/deno\.land\/x\/mesozoic@v[\w\.\-]+\//;
 
 /** `prepublish` will be invoked before publish, return `false` to prevent the publish. */
